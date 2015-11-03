4bb77dd951859fc83f2f23c9789105f8bac9c7dd
Parker Ciambrone <parkerreno@msn.com>
2015-11-03 14:28:40 -0800

    Added some background task stuff - needs to be triggered still (and actually do stuff).

---
+++ FoodTile/BackgroundUpdater/BalanceCheck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace BackgroundUpdater
+{
+    public sealed class BalanceCheck : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral _defferal = taskInstance.GetDeferral();
+
+            _defferal.Complete();
+        }
+    }
+}
