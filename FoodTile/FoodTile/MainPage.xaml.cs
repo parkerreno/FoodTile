8e96acb89e8ed513661d05f5d2ab65818d995ab7
Parker Ciambrone <parkerreno@msn.com>
2015-11-03 10:59:58 -0800

    Tile updating button + toast button and method stub

--- FoodTile/FoodTile/MainPage.xaml.cs
+++ FoodTile/FoodTile/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using NotificationsExtensions.Tiles;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +28,58 @@ namespace FoodTile
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var bindingContent = new TileBindingContentAdaptive();
+            var items = bindingContent.Children;
+
+            items.Add(new TileText()
+            {
+                Text = "Balance: $568.12",
+                Style = TileTextStyle.Subtitle
+            });
+
+            items.Add(new TileText()
+            {
+                Text = "Days: 47",
+                Style = TileTextStyle.Caption
+            });
+
+            items.Add(new TileText()
+            {
+                Text="Avg/Day: $12.42",
+                Style = TileTextStyle.Caption 
+            });
+
+            var tileBinding = new TileBinding();
+
+            tileBinding.Content = bindingContent;
+            tileBinding.DisplayName = "FoodTile";
+            tileBinding.Branding = TileBranding.NameAndLogo;
+
+            var content = new TileContent()
+            {
+                Visual = new TileVisual()
+                {
+                    TileMedium = tileBinding,
+                    TileWide = tileBinding,
+                    TileLarge = tileBinding
+                }
+            };
+
+            XmlDocument doc = content.GetXml();
+
+            var note = new TileNotification(doc);
+            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+
+            updater.Update(note);
+        }
+
+        private void Button_Tapped_1(object sender, TappedRoutedEventArgs e)
+        {
+
         }
     }
 }
