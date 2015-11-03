88700633c8abb4d449d7650bb720d00cf37321cc
Parker Ciambrone <parkerreno@msn.com>
2015-11-03 11:37:58 -0800

    Classes to deserialize data to added.  Will be moved later.

---
+++ FoodTile/FoodTile/json.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodTile
+{
+
+    public class Rootobject
+    {
+        public HuskyCard employee_husky_card { get; set; }
+        public HuskyCard resident_dining { get; set; }
+        public HuskyCard student_husky_card { get; set; }
+    }
+
+    public class HuskyCard
+    {
+        public float balance { get; set; }
+        public string last_updated { get; set; }
+        public string add_funds_url { get; set; }
+    }
+}
