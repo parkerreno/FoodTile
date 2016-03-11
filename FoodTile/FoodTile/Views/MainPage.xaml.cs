608fc32b0af05db30d099a9bd6e93e9da6fffd9f
Parker Ciambrone <parkerreno@msn.com>
2016-03-10 16:06:14 -0800

    Hooked up some more things for the 'real' app interface.

--- FoodTile/FoodTile/Views/MainPage.xaml.cs
+++ FoodTile/FoodTile/Views/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,9 +28,31 @@ namespace FoodTile.Views
             this.InitializeComponent();
         }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            UpdateData();
+            base.OnNavigatedTo(e);
+        }
+
         private void Adjust_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Frame.Navigate(typeof (Adjust));
+        }
+
+        private async void UpdateData()
+        {
+            if (App.MainViewModel.HfsData!=null||await App.MainViewModel.GetData())
+            {
+                TotalBlock.Text = $"{App.MainViewModel.HfsData.resident_dining.balance:C}";
+                AvgSpendBlock.Text = $"{App.MainViewModel.AverageSpend:C}";
+                DaysBlock.Text = $"{App.MainViewModel.TermInfo.FullDaysRemaining} days";
+            }
+            else
+            {
+                await new MessageDialog(
+                    "We couldn't get update information...  Please make sure you have internet and have not changed your password lately.",
+                    "Error Getting Data").ShowAsync();
+            }
         }
     }
 }
